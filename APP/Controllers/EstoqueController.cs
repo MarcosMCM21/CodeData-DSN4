443615764b7207c0a846faa217ce236a1a7c333b
Marcos Correa de Melo <marcoscorreademelo@gmail.com>
2024-09-27 11:56:48 -0300

    Mostrar status do equipamento Estoque/Locação/Homologação

--- APP/Controllers/EstoqueController.cs
+++ APP/Controllers/EstoqueController.cs
@@ -1,4 +1,6 @@
 ﻿using CodeData_Connection.Areas.Identity.Data;
+using CodeData_Connection.Models;
+using CodeData_Connection.Models.Database.Entidade;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol;
@@ -17,15 +19,36 @@ namespace CodeData_Connection.Controllers
         public async Task<IActionResult> Index()
         {
             var equipamentos = await _context.Equipamentos.ToListAsync();
+            var equipamentosStatus = new List<EquipamentoStatus>();
 
             if (equipamentos == null)
             {
                 Console.WriteLine("Nenhum equipamento encontrado!");
-
-                return View("Nenhum equipamento encontrado!");
             }
 
-            return View(equipamentos);
+            foreach (var equipamento in equipamentos)
+            {
+                try
+                {
+                    var status = _context.MovimentacoesEquipamentos
+                    .Join(
+                        _context.Solicitacoes,
+                        me => me.SolicitacaoId,
+                        s => s.Id,
+                        (me, s) => new { me.EquipamentoId, s.Tipo }
+                        )
+                        .Where(joined => joined.EquipamentoId == equipamento.Id)
+                        .ToList();
+
+                    equipamentosStatus.Add(new EquipamentoStatus() { Equipamento = equipamento, Status = status[0].Tipo ? "LOCAÇÃO" : "HOMOLOGAÇÃO" });
+                }
+                catch (Exception ex) 
+                {
+                    equipamentosStatus.Add(new EquipamentoStatus() { Equipamento = equipamento, Status = "ESTOQUE" });
+                }               
+            }
+
+            return View(equipamentosStatus);
         }
 
         //public JsonResult GetEquipamentoId(int id)
---
+++ APP/Models/EquipamentoStatus.cs
@@ -0,0 +1,10 @@
+﻿using CodeData_Connection.Models.Database.Entidade;
+
+namespace CodeData_Connection.Models
+{
+    public class EquipamentoStatus
+    {
+        public Equipamento Equipamento { get; set; }
+        public string Status { get; set; }
+    }
+}

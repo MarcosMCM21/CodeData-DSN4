9e3a5b8fef4d608ea48ca2606d3cbe780da888e8
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-10-16 22:07:38 -0300

    Adicionado a coluna de Nível de Acesso e a página de detalhes em Usuario

--- APP/Controllers/ConfiguracaoController.cs
+++ APP/Controllers/ConfiguracaoController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CodeData_Connection.Areas.Identity.User;
+using CodeData_Connection.Areas.Identity.Enums;
+using Microsoft.AspNetCore.Identity;
+using CodeData_Connection.Models;
 
 namespace CodeData_Connection.Controllers
 {
@@ -27,9 +31,89 @@ namespace CodeData_Connection.Controllers
 
         public async Task<IActionResult> ObterDadosUsuario()
         {
+            var dadosUsuarios = new List<DadosUsuario>();
+
             var usuarios = await _context.ApplicationUser.ToListAsync();
 
-            return PartialView("_DadosUsuario", usuarios);
+            if (usuarios == null)
+            {
+                return NotFound("Usuários não encontrado.");
+            }
+
+            foreach (var usuario in usuarios)
+            {
+                var roleUser = await _context.UserRoles.Where(r => r.UserId == usuario.Id).FirstOrDefaultAsync();
+                var role = await _context.Roles.Where(r => r.Id == roleUser.RoleId).FirstOrDefaultAsync();
+                dadosUsuarios.Add(new DadosUsuario { Usuario = usuario, NivelAcesso = role.Name });
+            }
+
+            return PartialView("_DadosUsuario", dadosUsuarios);
         }
+
+        public IActionResult DetalhesUsuario(string id)
+        {
+            // 1. Buscar o usuario pelo ID, incluindo os dados relacionados (se necessário)
+            var dadosUsuario = ObterDetalhesUsuario(id);
+
+            if (dadosUsuario == null)
+            {
+                return NotFound("Cliente não encontrado.");
+            }
+
+            return View(dadosUsuario);
+        }
+
+        public DetalhesUsuarioViewModel ObterDetalhesUsuario(string usuarioId)
+        {
+            // 1. Obter a solicitação correspondente ao ID
+            var usuario = _context.Users.FirstOrDefault(c => c.Id == usuarioId);
+
+            if (usuario == null)
+            {
+                return null; // ou lançar uma exceção se preferir
+            }
+
+            // 2. Obter as solicitações associados ao cliente
+            List<DadosSolicitacao> dadosSolicitacoes = new List<DadosSolicitacao>();
+            var solicitacoes = _context.Solicitacoes
+                .Include(c => c.Cliente)
+                .Where(s => s.UserId == usuarioId)
+                .ToList();
+
+            foreach (var solicitacao in solicitacoes)
+            {
+                dadosSolicitacoes.Add(new DadosSolicitacao
+                {
+                    Solicitacao = solicitacao,
+                    Cliente = solicitacao.Cliente.Nome,
+                    Vendedor = ""
+                });
+            }
+
+            var gerente = _context.Users.Where(g => g.Id == usuario.GerenteID).FirstOrDefault();
+
+            // 3. Criar a instância do ViewModel e popular suas propriedades
+            var detalhesUsuario = new DetalhesUsuarioViewModel
+            {
+                Usuario = usuario,
+                Solicitacoes = dadosSolicitacoes,
+                Gerente = gerente
+            };
+
+            return detalhesUsuario; // Retorna o ViewModel preenchido
+        }
+    }
+
+    public class DetalhesUsuarioViewModel
+    {
+        public ApplicationUser Usuario { get; set; }
+        public ApplicationUser? Gerente { get; set; }
+        public List<DadosSolicitacao> Solicitacoes { get; set; }
+    }
+
+    public class DadosUsuario
+    {
+        public ApplicationUser Usuario { get; set; }
+        public string? NivelAcesso { get; set; }
     }
 }
